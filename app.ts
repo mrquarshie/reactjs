0a93901e847108d89f05bc5bc1eb5a7afe0efdb2
quarshie.py <52331668+mrquarshie@users.noreply.github.com>
2025-08-22 13:38:43 +0000

    lil code

--- app.ts
+++ app.ts
@@ -1,2 +1,10 @@
 let firstName: string= "dylan";
 let lastName = "Dylan";
+//type: unknown
+let w: unknown=1;
+w= "string";
+w= {
+    runANonExistentMethod: () => {
+        console.log("i think therefore i am")
+    }
+}
